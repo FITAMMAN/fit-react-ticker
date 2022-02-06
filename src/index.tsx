0cf27090a14087d2b22a7ba2f2561bc0772c5fc0
FIT <99121478+FITAMMAN@users.noreply.github.com>
2022-02-06 16:11:51 +0200

    Add files via upload

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import FinancialTicker from './tickers/FinancialTicker';
+import NewsTicker from './tickers/NewsTicker';
+import Ticker from './tickers/Ticker';
+
+export {
+  FinancialTicker, NewsTicker
+};
+
+export default Ticker;
